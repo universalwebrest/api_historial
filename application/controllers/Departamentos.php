37f3312f96d24e5bc00fd2b5d11087808d6a2038
hijujuy <diego@pcmint>
2020-04-14 12:01:01 -0300

    Api CRUD Departamentos

---
+++ application/controllers/Departamentos.php
@@ -0,0 +1,107 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . '/libraries/REST_Controller.php';
+require_once APPPATH . '/libraries/Format.php';
+
+class Departamentos extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('departamentos_model');        
+    }
+    
+    public function index_get()
+    {
+        $departamentos = $this->departamentos_model->get();
+        
+        if (!is_null($departamentos))
+        {
+            $this->response(array('departamentos' => $departamentos), 200);
+        }else
+        {
+            $this->response(array('error' => 'No existen departamentos registrados ...'), 404);
+        }
+    }
+    
+    public function find_get($id = null)
+    {
+        if (is_null($id))
+        {
+            $this->response(array("error" => "Debe proporcionar valor ID del departamento"), 400);
+        }
+        
+        $departamento = $this->departamentos_model->get($id);
+        
+        if (!is_null($departamento))
+        {
+            $this->response(array('departamento' => $departamento) , 200);
+        }
+        else
+        {
+            $this->response(array('error' => 'Departamento no encontrado'), 404);
+        }
+    }
+    
+    public function index_post()
+    {
+        if (!$this->post('departamento'))
+        {
+            $this->response(array('error' => 'No hay departamento para registrar'), 400);
+        }
+        
+        $id = $this->departamentos_model->save($this->post('departamento'));
+        
+        if (!is_null($id))
+        {
+            $this->response(array('id' => $id), 200);
+        }else
+        {
+            $this->response(array('error' => 'Surgio un error en el servidor'), 400);
+        }
+        
+    }
+    
+    public function update_post($id)
+    {
+        if (!$this->post('data') || !$id)
+        {
+            $this->response(null, 400);
+        }
+        
+        $update = $this->departamentos_model->update($id, $this->post('data'));
+        
+        if ($update)
+        {
+            $this->response(array('response' => 'El departamento fue actualizado satisfactoriamente'), 200);
+        }
+        else
+        {
+            $this->response(array('error' => 'Surgio un error en el servidor'), 400);
+        }
+    }
+    
+    public function index_delete($id = null)
+    {
+        if (is_null($id))
+        {
+            $this->response(array("error" => "El ID fue null"), 400);
+        }
+        
+        $delete = $this->departamentos_model->delete($id);
+        
+        if ($delete)
+        {
+            $this->response(array('response' => 'El departamento fue eliminado satisfactoriamente'), 200);
+        }
+        else
+        {
+            $this->response(array('error' => 'Surgio un error en el servidor'), 400);
+        }
+    }
+    
+    
+}
+
---
+++ application/models/Departamentos_model.php
@@ -0,0 +1,88 @@
+<?php
+
+class Departamentos_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();        
+    }
+    
+    public function get($id = null)
+    {        
+        $this->db->select('ID, DESCRIPCION');
+        
+        if (is_null($id))
+        {
+            $query = $this->db->get('DEPARTAMENTO');
+            
+            if ($query->num_rows() > 0)
+            {
+                return $query->result();
+            }
+            
+            return null;
+        }
+        else 
+        {
+            $this->db->where('ID', $id);
+            
+            $query = $this->db->get('DEPARTAMENTO');
+            
+            if ($query->num_rows() == 1)
+            {
+                return $query->row();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    
+    public function save($departamento)
+    {
+        $array = array('ID'=>$departamento['id'],
+                                      'DESCRIPCION'=>$departamento['descripcion']);
+        
+        $this->db->set($array);
+        
+        $this->db->insert('DEPARTAMENTO');
+        
+        if ($this->db->affected_rows() == 1)
+        {
+            return $this->db->insert_id();
+        }
+        
+        return FALSE;
+    }
+    
+    public function update($id, $data)
+    {
+        $this->db->where('ID', $id);
+        
+        $this->db->update('DEPARTAMENTO', $data['key_value']);
+        
+        if ($this->db->affected_rows() == 1)
+        {
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+    
+    public function delete($id)
+    {       
+        $this->db->where('ID', $id);
+        
+        $this->db->delete('DEPARTAMENTO');
+        
+        if ($this->db->affected_rows() == 1)
+        {
+            return TRUE;
+        }
+        
+        return FALSE;
+    }
+    
+}
+
