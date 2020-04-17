6ff9dc72d10607459f576fc33ebf72c46bf49a0c
Diego <dmchumacero@gmail.com>
2020-04-17 00:03:33 -0300

    Modelo de objetos Localidad

--- application/models/Localidades_model.php
+++ application/models/Localidades_model.php
@@ -9,6 +9,7 @@ class Localidades_model extends CI_Model
     
     public function get($id = null)
     {
+        // Opcion para cargar todos los registros de LOCALIDAD
         if (is_null($id))
         {
             $localidades = $this->db->get('LOCALIDAD');
@@ -22,21 +23,68 @@ class Localidades_model extends CI_Model
                 return null;
             }
         }
+        else //Opcion para cargar solo un regitro por medio del ID pasado desde el controller
+        {
+            $this->db->from('LOCALIDAD');
+            
+            $this->db->select('ID, DESCRIPCION, CODIGO_POSTAL, DEPARTAMENTO_ID');
+            
+            $this->db->where('ID', $id);
+            
+            $localidad = $this->db->get();
+            
+            return $localidad->row();
+        }
     }
     
     public function save($localidad)
     {
+        $data = $this->_setLocalidad($localidad);
         
+        $this->db->insert("LOCALIDAD", $data);
+        
+        if ($this->db->affected_rows() == 1)
+        {
+            return $this->db->insert_id();
+        }
+        else 
+        {
+            return null;
+        }
     }
     
-    public function update($id, $localidad)
+    public function update($id, $data)
     {
+        $this->db->where('ID', $id);
         
+        $this->db->update('LOCALIDAD', $data);
+        
+        if ($this->db->affected_rows() == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     
     public function delete($id)
     {
-                
+        $this->db->where('ID', $id);
+        
+        $this->db->delete('LOCALIDAD');
+        
+        if ($this->db->affected_rows() == 1) return TRUE;
+        else return FALSE;
     }
+    
+    public function _setLocalidad($localidad)
+    {
+        return array("DESCRIPCION" => $localidad['descripcion'],
+                     "CODIGO_POSTAL" => $localidad['codigo_postal'],
+                     "DEPARTAMENTO_ID" => $localidad['departamento_id']);
+    }
+    
 }
 
